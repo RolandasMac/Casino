fd2103d7d64a58fe1671cb46fdd8aa1ed8d98545
Rolandas Macius <rolandas.macius@gmail.com>
2024-05-15 20:42:18 +0300

    Pirmas commit'as

---
+++ js/script.js
@@ -0,0 +1,60 @@
+const emoji = [
+    "🍇",
+    "🍅",
+    "🍒",
+    "🌰",
+    "🍌",
+    "🍆",
+    "🍄",
+    "🥔",
+    "☘️",
+    "🍗",
+    "🎰"
+
+
+]
+let dispBox = document.querySelectorAll('.box');
+const btnSukti = document.querySelector('#sukti');
+
+initGame(emoji);
+
+function initGame(arr){
+    let count = arr.length-1;
+    console.log(count)
+    for(let g=0; g<3; g++){
+
+        let arr1=[...arr];
+        let startItem = arr1.splice(10, 1);
+        console.log(startItem)
+        for(let i = count; i >=0; i--) {
+            console.log(i)
+            let randomInt = Math.floor(Math.random() * arr1.length );
+            const div = document.createElement('div');
+            const span = document.createElement('span');
+            if(i===0){
+                span.innerHTML += startItem[0];
+            }else{
+                span.innerHTML += arr1.splice(randomInt, 1)
+            }
+            div.appendChild(span);
+            dispBox[g].appendChild(div);
+        }
+    }
+
+}
+
+
+btnSukti.addEventListener('click', sukti);
+async function sukti() {
+    for(let g=0; g<3; g++) {
+        for (let i = 0; i < emoji.length; i++) {
+            // disp[0].children[i].style.top = top + `px`
+            dispBox[g].children[i].style.transform = `translateY(900%)`;
+        }
+        await new Promise((res)=>{
+            setTimeout(()=>{
+                res()
+            },100)
+        })
+    }
+}
